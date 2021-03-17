5ca55af5c0949bb1ab5bd9333e559377498d9c30
A01634956 <a01634956@itesm.mx>
2021-03-17 14:45:05 +0000

    Practica 3

---
+++ IPC/signal.c
@@ -0,0 +1,22 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int varGlobal;
+
+void signalHandler(int sig){
+  printf("Recibi segnal %d\n", sig);
+  varGlobal=0;
+}
+
+int main(){
+  varGlobal=1;
+  signal(2, signalHandler);
+  signal(15, signalHandler);
+  while(varGlobal){
+    printf("Trabajando\n");
+    sleep(1);
+  }
+  printf("Ya voy a terminar \n");
+  return 0;
+}
